dd7ef5158f01d8549eb315f0cf3c6740a42bcfb6
Jane Jeyranyan <73958620+janejey@users.noreply.github.com>
2021-02-28 23:14:41 +0400

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,27 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+function nextAndPreviousSlide(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// function playAndPause {play, pause} {
+//   let play = document.getElementsByClassName("play");
+//   let pause = document.getElementsByClassName("pause");
+//   if () {}
+// }
